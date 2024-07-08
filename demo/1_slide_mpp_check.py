9589b2cd4f4680a502c65c9a7d61b844836190b2
George Batchkala <george.batchkala@gmail.com>
2024-07-08 18:29:20 +0100

    use `./sample_data` directory instead of `~./cache`
    this way, the downloaded files are very visible and will not be left in  `~./cache` by accedent

--- demo/1_slide_mpp_check.py
+++ demo/1_slide_mpp_check.py
@@ -5,10 +5,9 @@ from gigapath.preprocessing.data.slide_utils import find_level_for_target_mpp
 homedir_path = os.path.expanduser("~")
 assert ("HF_TOKEN" in os.environ) or os.path.exists(f"{homedir_path}/.cache/huggingface/token"), "Please set the HF_TOKEN environment variable to your Hugging Face API token or make sure the token is cached in ~/.cache/huggingface/token"
 
-local_dir = os.path.join(os.path.expanduser("~"), ".cache/")
-huggingface_hub.hf_hub_download("prov-gigapath/prov-gigapath", filename="sample_data/PROV-000-000001.ndpi", local_dir=local_dir, force_download=True)
-
-slide_path = os.path.join(local_dir, "sample_data/PROV-000-000001.ndpi")
+local_dir = "./sample_data/"
+slide_path = os.path.join(local_dir, "PROV-000-000001.ndpi")
+huggingface_hub.hf_hub_download("prov-gigapath/prov-gigapath", filename=slide_path, local_dir=".", force_download=True)
 
 print("NOTE: Prov-GigaPath is trained with 0.5 mpp preprocessed slides")
 target_mpp = 0.5
